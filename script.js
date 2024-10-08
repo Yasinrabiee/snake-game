43b56961657f5c24edbacd7e168d4a592c9b0b28
Yasin Rabiee <hlww6303@gmail.com>
2024-10-08 11:59:04 -0700

    add updateDirection function

--- script.js
+++ script.js
@@ -9,18 +9,46 @@ let scale = 10;
 
 function Snake() {
 	this.x = 0;
-	this.y = 0;
+	this.y = 10;
 	this.xSpeed = scale;
 	this.ySpeed = 0;
+
 	this.snakeDraw = function() {
 		ctx.fillStyle = `#FFF`;
 		ctx.fillRect(this.x , this.y, scale, scale);
 	}
+
 	this.updateLocation = function() {
 		this.x += this.xSpeed;
 		this.y += this.ySpeed;
 		if(this.x > canvasWidth)
 			this.x = 0;
+	}
+
+	this.updateDirection = function(userDirection) {
+		switch(userDirection)
+		{
+			case 'Up': {
+				this.xSpeed = 0;
+				this.ySpeed = -scale;
+				break;
+			}
+			case 'Down': {
+				this.xSpeed = 0;
+				this.ySpeed = scale;
+				break;
+			}
+			case 'Left': {
+				this.xSpeed = -scale;
+				this.ySpeed = 0;
+				break;
+			}
+			case 'Right': {
+				this.xSpeed = scale;
+				this.ySpeed = 0;
+				break;
+			}
+		}
 	}
 }
 
@@ -31,3 +59,9 @@ setInterval(function() {
 	snake.snakeDraw();
 	snake.updateLocation();
 },150);
+
+window.addEventListener(`keydown`, function(event) {
+	const userDirection = event.key.replace(`Arrow`, ``);
+	console.log(userDirection);
+	snake.updateDirection(userDirection);
+});
