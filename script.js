eb22dd5c4efbbd86c83db59107a3dadf820e1128
Yasin Rabiee <hlww6303@gmail.com>
2024-10-08 11:16:55 -0700

    add snake function

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+function random(min, max) {
+	return Math.floor(Math.random()*(max-min+1)) + min;
+}
+
 const canvas = document.querySelector(`#canvas`);
 const ctx = canvas.getContext(`2d`);
+const canvasWidth = document.querySelector(`#canvas`).offsetWidth;
+let scale = 10;
 
-let position = 0;
+function Snake() {
+	this.x = 0;
+	this.y = 0;
+	this.xSpeed = scale;
+	this.ySpeed = 0;
+	this.snakeDraw = function() {
+		ctx.fillStyle = `#FFF`;
+		ctx.fillRect(this.x , this.y, scale, scale);
+	}
+	this.updateLocation = function() {
+		this.x += this.xSpeed;
+		this.y += this.ySpeed;
+		if(this.x > canvasWidth)
+			this.x = 0;
+	}
+}
+
+let snake = new Snake();
 
 setInterval(function() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillRect(0 + position, 0, 10, 10);
-	if(position > canvas.width)
-		position = 0;
-	position += 10;
-},250);
+	snake.snakeDraw();
+	snake.updateLocation();
+},150);
