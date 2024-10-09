de934339147df9e038a86a819b1b7bf24b1d4b29
Yasin Rabiee <hlww6303@gmail.com>
2024-10-09 13:23:03 -0700

    add new feature: control snake

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
-function random(min, max) {
-	return Math.floor(Math.random()*(max-min+1)) + min;
+function rand(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const canvas = document.querySelector(`#canvas`);
 const ctx = canvas.getContext(`2d`);
 const canvasWidth = document.querySelector(`#canvas`).offsetWidth;
+const canvasHeight = document.querySelector(`#canvas`).offsetHeight;
 let scale = 10;
 
 function Snake() {
 	this.x = 0;
-	this.y = 10;
+	this.y = 0;
 	this.xSpeed = scale;
 	this.ySpeed = 0;
 
 	this.snakeDraw = function() {
 		ctx.fillStyle = `#FFF`;
 		ctx.fillRect(this.x , this.y, scale, scale);
-	}
-
-	this.updateLocation = function() {
 		this.x += this.xSpeed;
 		this.y += this.ySpeed;
 		if(this.x > canvasWidth)
 			this.x = 0;
+		else if(this.x < 0)
+		 	this.x = canvasWidth;
+		else if(this.y > canvasHeight)
+		 	this.y = 0;
+		else if(this.y < 0)
+			this.y = canvasHeight;
 	}
+
+	// this.updateLocation = function() {
+	// 	this.x += this.xSpeed;
+	// 	this.y += this.ySpeed;
+	// 	if(this.x > canvasWidth)
+	// 		this.x = 0;
+	// }
 
 	this.updateDirection = function(userDirection) {
 		switch(userDirection)
 		{
-			case 'Up': {
+			case 'Up':
+			{
 				this.xSpeed = 0;
 				this.ySpeed = -scale;
 				break;
 			}
-			case 'Down': {
+			case 'Down': 
+			{
 				this.xSpeed = 0;
 				this.ySpeed = scale;
 				break;
 			}
-			case 'Left': {
+			case 'Left': 
+			{
 				this.xSpeed = -scale;
 				this.ySpeed = 0;
 				break;
 			}
-			case 'Right': {
+			case 'Right': 
+			{
 				this.xSpeed = scale;
 				this.ySpeed = 0;
 				break;
@@ -52,16 +67,14 @@ function Snake() {
 	}
 }
 
-let snake = new Snake();
+const snake = new Snake();
 
 setInterval(function() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	snake.snakeDraw();
-	snake.updateLocation();
 },150);
 
 window.addEventListener(`keydown`, function(event) {
 	const userDirection = event.key.replace(`Arrow`, ``);
-	console.log(userDirection);
 	snake.updateDirection(userDirection);
 });
